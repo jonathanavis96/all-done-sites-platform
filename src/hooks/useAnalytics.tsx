25f28d0249c1da4516e37f8f8d7ec72ef641ff80
jonathanavis96 <jonathanavis96@gmail.com>
2025-08-25 12:57:15 +0200

    Create useAnalytics.tsx

---
+++ src/hooks/useAnalytics.tsx
@@ -0,0 +1,22 @@
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
+declare global {
+  interface Window {
+    gtag?: (...args: any[]) => void;
+  }
+}
+
+export function useAnalytics() {
+  const location = useLocation();
+
+  useEffect(() => {
+    if (window.gtag) {
+      window.gtag("event", "page_view", {
+        page_path: location.pathname + location.search,
+        page_location: window.location.href,
+        page_title: document.title,
+      });
+    }
+  }, [location]);
+}
